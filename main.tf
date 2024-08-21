8c3ba75d26c4aada81ae1d90e7dbe224171324b5
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-22 00:22:29 +0530

    Corrected output and formatted files

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_providers {
-        aws = {
-            version = ">=5.61.0"
-            source = "hashicorp/aws"
-        }
+  required_providers {
+    aws = {
+      version = ">=5.61.0"
+      source  = "hashicorp/aws"
     }
+  }
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami = var.ami
-    instance_type = var.instance_type
-    tags = local.tags
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags          = local.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "ami" {
-    type = string
-    default = "ami-0b72821e2f351e396"
+  type    = string
+  default = "ami-0b72821e2f351e396"
 }
 
 variable "region" {
-    type    =   string
-    default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
 locals {
-    tags = {
-        "Name" = "test-ec2"
-        "Creation-Date" = formatdate("DD-MM-YYYY hh:mm:ss ZZZ", timestamp())
-    }
+  tags = {
+    "Name"          = "test-ec2"
+    "Creation-Date" = formatdate("DD-MM-YYYY hh:mm:ss ZZZ", timestamp())
+  }
 }
