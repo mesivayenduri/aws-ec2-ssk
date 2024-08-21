db05b9815db63fec0ba508b8369f9a5994e77877
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-22 00:11:37 +0530

    aws-ec2-ssk Module

---
+++ README.md
@@ -0,0 +1,2 @@
+This module create a ec2 instance (default) in us-east-1(defaut) region.
+Outputs InstaneId, privaetIP, publicIP, Name of the Instance
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+    type = string
+    default = "ami-0b72821e2f351e396"
+}
+
+variable "region" {
+    type    =   string
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+locals {
+    tags = {
+        "Name" = "test-ec2"
+        "Creation-Date" = formatdate("DD-MM-YYYY hh:mm:ss ZZZ", timestamp())
+    }
+}
